2e6bf84824f187b09733ba19500d3349fe91a1a0
Avichal-Goyal <avichalgoyal2105@gmail.com>
2025-02-09 01:25:27 +0530

    create a constructor func

--- OOPs/createConstructorFunc.js
+++ OOPs/createConstructorFunc.js
@@ -1,10 +1,13 @@
 {
-    function User(name){
-        console.log(this);
+    function User(name, interests){
         this.name = name;
+        this.interests = interests;
         console.log(this);
     }
 
-    let me = new User();
+    let me = new User("Avichal", ["sports", "movies", "anime"]);
+    let you = new User("Cassy");
+    console.log(me, you);
+    me.membership = "Platinum";
     console.log(me);
 }
